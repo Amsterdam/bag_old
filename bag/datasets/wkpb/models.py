3ce221a10c74cfc6e3fb6ea7c7fee4cf5dfe3976
Marten Hoekstra <marten@dolhuis.nl>
2019-09-26 15:40:01 +0200

    Restore external download facilities

--- bag/datasets/wkpb/models.py
+++ bag/datasets/wkpb/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from bag.settings import settings
 from datasets.bag import models as bag
 from datasets.brk import models as brk
 from datasets.generic import mixins
@@ -99,6 +100,10 @@ class Brondocument(models.Model):
     def url(self):
         base_url = 'http://diva.intranet.amsterdam.nl/Brondocumenten/Wkpb'
         return '%s/%s' % (base_url, self.documentnaam)
+
+    @property
+    def extern_url(self):
+        return f'{settings.DATAPUNT_API_URL}wkpb/brondocument/{self.id}/?as_pdf'
 
     class Meta:
         verbose_name = "Brondocument"
